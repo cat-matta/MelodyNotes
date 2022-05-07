441a2cf32f94f39fa1a98b6762440b2126ba6829
Catherine <catherine.matta22399@gmail.com>
2022-05-06 21:08:33 -0400

    Initial UI laid

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,147 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'themedata.dart';
+
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+  runApp(AppEntry());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class AppEntry extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: AppTheme.maintheme(),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(child: TopBar()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final mediaQuerry = MediaQuery.of(context);
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        appBar: AppBar(
+            centerTitle: true,
+            titleSpacing: 2.3,
+            title: Container(
+                // alignment: Alignment.center,
+                width: mediaQuerry.size.width * 0.5,
+                decoration: BoxDecoration(
+                    color: AppTheme.lightBackground,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () => {},
+                      icon: Icon(Icons.settings),
+                      color: AppTheme.accentSecondary,
+                    ),
+                    Text(
+                      "Piece",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    IconButton(
+                      onPressed: () => {},
+                      icon: Icon(Icons.search),
+                      color: AppTheme.accentSecondary,
+                    ),
+                  ],
+                )),
+            leadingWidth: mediaQuerry.size.width,
+            leading: Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.display_settings),
+                  color: AppTheme.maintheme().iconTheme.color,
+                  onPressed: () {
+                    // handle the press
+                  },
+                ),
+                Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.music_note),
+                  color: AppTheme.maintheme().iconTheme.color,
+                  onPressed: () {
+                    // handle the press
+                  },
+                ),
+                Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.bookmarks),
+                  color: AppTheme.maintheme().iconTheme.color,
+                  onPressed: () {
+                    // handle the press
+                  },
+                ),
+                Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.collections_bookmark),
+                  color: AppTheme.maintheme().iconTheme.color,
+                  onPressed: () {
+                    // handle the press
+                  },
+                ),
+                Spacer(),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // Spacer(),
+                  IconButton(
+                    icon: const Icon(Icons.draw),
+                    tooltip: 'Open shopping cart',
+                    onPressed: () {
+                      // handle the press
+                    },
+                  ),
+                  // Spacer(),
+                  IconButton(
+                    icon: const Icon(Icons.library_music),
+                    tooltip: 'Open shopping cart',
+                    onPressed: () {
+                      // handle the press
+                    },
+                  ),
+                  // Spacer(),
+                  IconButton(
+                    icon: const Icon(Icons.photo_camera),
+                    tooltip: 'Open shopping cart',
+                    onPressed: () {
+                      // handle the press
+                    },
+                  ),
+                  // Spacer(),
+                  IconButton(
+                    icon: const Icon(Icons.menu),
+                    tooltip: 'Open shopping cart',
+                    onPressed: () {
+                      // handle the press
+                    },
+                  ),
+                  // Spacer(),
+                ],
+              )
+            ]),
+        body: AppBody());
+  }
+}
+
+class AppBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
