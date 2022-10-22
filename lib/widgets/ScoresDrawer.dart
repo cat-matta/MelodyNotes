79c3ad4d3371499ae6112b7613a93c38622acea0
baishakicodes <bdebi5985@bths.edu>
2022-10-22 11:04:36 -0400

    testing pdftron

--- lib/widgets/ScoresDrawer.dart
+++ lib/widgets/ScoresDrawer.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:musescore/themedata.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:pdftron_flutter/pdftron_flutter.dart';
+import 'package:flutter/services.dart';
 
 import './pdf_viewer_page.dart';
+import 'dart:io' show Platform;
 import 'dart:io';
 
 class ScoreDrawer extends StatefulWidget {
@@ -16,6 +19,8 @@ class _ScoresLibraryWidgetState extends State<ScoreDrawer> {
   // String remotePDFpath = "";
   // String corruptedPathPDF = "";
   //final scaffoldKey = GlobalKey<ScaffoldState>();
+  String _version = 'Unknown';
+  bool _showViewer = true;
 
   //This is for changing color in Second Nav bar each button
   bool _hasBeenPressedComposer = true;
@@ -36,30 +41,28 @@ class _ScoresLibraryWidgetState extends State<ScoreDrawer> {
   //Same from SetlistDrawer.dart
   late TextEditingController _controller;
 
-  //variable to initialize file picker object & hold the file object
-  // FilePickerResult? result;
-  // // PlatformFile? file;
-  // File? file;
+  Future<void> initPlatformState() async {
+    String version;
+    // Platform messages may fail, so use a try/catch PlatformException.
+    try {
+      // Initializes the PDFTron SDK, it must be called before you can use
+      // any functionality.
+      PdftronFlutter.initialize("your_pdftron_license_key");
 
-  // Widget fileDetails(PlatformFile file) {
-  //   final kb = file.size / 1024;
-  //   final mb = kb / 1024;
-  //   final size = (mb >= 1)
-  //       ? '${mb.toStringAsFixed(2)} MB'
-  //       : '${kb.toStringAsFixed(2)} KB';
-  //   return Padding(
-  //     padding: const EdgeInsets.all(8.0),
-  //     child: Column(
-  //       crossAxisAlignment: CrossAxisAlignment.start,
-  //       children: [
-  //         Text('File Name: ${file.name}'),
-  //         Text('File Size: $size'),
-  //         Text('File Extension: ${file.extension}'),
-  //         Text('File Path: ${file.path}'),
-  //       ],
-  //     ),
-  //   );
-  // }
+      version = await PdftronFlutter.version;
+    } on PlatformException {
+      version = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, you want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _version = version;
+    });
+  }
 
   //method called when the stateful widget is inserted in the widget tree
   //it will only run once and initilize and listeners/variables
@@ -67,6 +70,7 @@ class _ScoresLibraryWidgetState extends State<ScoreDrawer> {
   void initState() {
     super.initState();
     _controller = TextEditingController();
+    initPlatformState();
   }
 
   //to get rid of the controller when it is no longer needed
@@ -185,7 +189,8 @@ class _ScoresLibraryWidgetState extends State<ScoreDrawer> {
                       String url = '';
                       final file = await pickFile();
                       if (file == null) return;
-                      openPdf(context, file, url);
+                      url = file.path;
+                      openPdf2(context, file, url);
                       // result = await FilePicker.platform.pickFiles(
                       //   type: FileType.custom,
                       //   allowedExtensions: ['pdf'],
@@ -399,7 +404,6 @@ class _ScoresLibraryWidgetState extends State<ScoreDrawer> {
     return File(result.paths.first ?? '');
   }
 
-
   void openPdf(BuildContext context, File file, String url) =>
       Navigator.of(context).push(
         MaterialPageRoute(
@@ -409,4 +413,82 @@ class _ScoresLibraryWidgetState extends State<ScoreDrawer> {
           ),
         ),
       );
+
+  void openPdf2(BuildContext context, File file, String url) async {
+    // opening without a config file will have all functionality enabled.
+    // await PdftronFlutter.openDocument(_document);
+
+    var config = Config();
+    // How to disable functionality:
+    //      config.disabledElements = [Buttons.shareButton, Buttons.searchButton];
+    //      config.disabledTools = [Tools.annotationCreateLine, Tools.annotationCreateRectangle];
+    // Other viewer configurations:
+    //      config.multiTabEnabled = true;
+    //      config.customHeaders = {'headerName': 'headerValue'};
+
+    // An event listener for document loading
+    var documentLoadedCancel = startDocumentLoadedListener((filePath) {
+      print("document loaded: $filePath");
+    });
+
+    await PdftronFlutter.openDocument(url, config: config);
+
+    try {
+      // The imported command is in XFDF format and tells whether to add, modify or delete annotations in the current document.
+      PdftronFlutter.importAnnotationCommand(
+          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+              "    <xfdf xmlns=\"http://ns.adobe.com/xfdf/\" xml:space=\"preserve\">\n" +
+              "      <add>\n" +
+              "        <square style=\"solid\" width=\"5\" color=\"#E44234\" opacity=\"1\" creationdate=\"D:20200619203211Z\" flags=\"print\" date=\"D:20200619203211Z\" name=\"c684da06-12d2-4ccd-9361-0a1bf2e089e3\" page=\"1\" rect=\"113.312,277.056,235.43,350.173\" title=\"\" />\n" +
+              "      </add>\n" +
+              "      <modify />\n" +
+              "      <delete />\n" +
+              "      <pdf-info import-version=\"3\" version=\"2\" xmlns=\"http://www.pdftron.com/pdfinfo\" />\n" +
+              "    </xfdf>");
+    } on PlatformException catch (e) {
+      print("Failed to importAnnotationCommand '${e.message}'.");
+    }
+
+    try {
+      // Adds a bookmark into the document.
+      PdftronFlutter.importBookmarkJson('{"0":"Page 1"}');
+    } on PlatformException catch (e) {
+      print("Failed to importBookmarkJson '${e.message}'.");
+    }
+
+    // An event listener for when local annotation changes are committed to the document.
+    // xfdfCommand is the XFDF Command of the annotation that was last changed.
+    var annotCancel = startExportAnnotationCommandListener((xfdfCommand) async {
+      String command = xfdfCommand;
+      print("flutter xfdfCommand:\n");
+      // Dart limits how many characters are printed onto the console.
+      // The code below ensures that all of the XFDF command is printed.
+      if (command.length > 1024) {
+        int start = 0;
+        int end = 1023;
+        while (end < command.length) {
+          print(command.substring(start, end) + "\n");
+          start += 1024;
+          end += 1024;
+        }
+        print(command.substring(start));
+      } else {
+        print(command);
+      }
+    });
+
+    // An event listener for when local bookmark changes are committed to the document.
+    // bookmarkJson is JSON string containing all the bookmarks that exist when the change was made.
+    var bookmarkCancel = startExportBookmarkListener((bookmarkJson) {
+      print("flutter bookmark: $bookmarkJson");
+    });
+
+    var path = await PdftronFlutter.saveDocument();
+    print("flutter save: $path");
+
+    // To cancel event:
+    // annotCancel();
+    // bookmarkCancel();
+    // documentLoadedCancel();
+  }
 }
