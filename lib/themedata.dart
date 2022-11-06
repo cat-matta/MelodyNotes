fd254edc5b9d3168f3809cd98e98e140a810f19f
baishakicodes <bdebi5985@bths.edu>
2022-11-06 11:31:28 -0500

    submit button

--- lib/themedata.dart
+++ lib/themedata.dart
@@ -6,7 +6,7 @@ class AppTheme {
   static final accentSecondary = Color.fromRGBO(4, 67, 171, 1);
 
   static final darkBackground = Color.fromRGBO(44, 44, 60, 1);
-  static final lightBackground = Color.fromRGBO(234, 234, 235, 1);
+  static final lightBackground = Color.fromRGBO(255, 255, 255, 1);
   static final headerFontWeight = FontWeight.w600;
   static final headerFontSize = 30.0;
 
--- lib/widgets/SpecificationsDrawer.dart
+++ lib/widgets/SpecificationsDrawer.dart
@@ -26,234 +26,254 @@ class _SpecificationsWidget extends State<SpecificationsDrawer> {
       appBar: AppBar(
         title: Text('No Title'),
       ),
-      body: Padding(
-        padding: EdgeInsets.only(bottom: 20),
-        child: Table(
-          columnWidths: {
-            0: FlexColumnWidth(2),
-            1: FlexColumnWidth(1),
-            2: FlexColumnWidth(6),
-          },
-          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-          children: [
-            TableRow(children: [
-              Text(
-                "Title",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editTitle,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editTitle = value!;
-                  });
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(bottom: 20),
+          child: Column(
+            children: [
+              Table(
+                columnWidths: {
+                  0: FlexColumnWidth(2),
+                  1: FlexColumnWidth(1),
+                  2: FlexColumnWidth(6),
                 },
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                children: [
+                  TableRow(children: [
+                    Text(
+                      "Title",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editTitle,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editTitle = value!;
+                        });
+                      },
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'No Title',
+                      ),
+                    ),
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Composer",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editComposer,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editComposer = value!;
+                        });
+                      },
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'No Composer',
+                      ),
+                    ),
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Genres",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editGenres,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editGenres = value!;
+                        });
+                      },
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'No Genres',
+                      ),
+                    ),
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Tags",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editTags,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editTags = value!;
+                        });
+                      },
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'No Tags',
+                      ),
+                    ),
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Labels",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editLabels,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editLabels = value!;
+                        });
+                      },
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'No Labels',
+                      ),
+                    ),
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Reference",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editReference,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editReference = value!;
+                        });
+                      },
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'No Reference',
+                      ),
+                    ),
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Rating",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editRating,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editRating = value!;
+                        });
+                      },
+                    ),
+                    RatingBar.builder(
+                      initialRating: 0,
+                      minRating: 0,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: AppTheme.accentMain,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Difficulty",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editDifficulty,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editDifficulty = value!;
+                        });
+                      },
+                    ),
+                    RatingBar.builder(
+                      initialRating: 0,
+                      minRating: 0,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 3,
+                      itemBuilder: (context, _) => Icon(
+                        Icons.circle,
+                        color: AppTheme.accentMain,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Time",
+                      textAlign: TextAlign.right,
+                      textScaleFactor: 1.3,
+                    ),
+                    Checkbox(
+                      activeColor: AppTheme.accentMain,
+                      value: editTime,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          editTime = value!;
+                        });
+                      },
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'No Time',
+                      ),
+                    ),
+                  ]),
+                ],
               ),
-              TextField(
-                textAlign: TextAlign.left,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'No Title',
+              TextButton(
+                onPressed: () => {},
+                child: const Text(
+                  'Submit',
+                  overflow: TextOverflow.ellipsis,
+                ),
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(
+                    fontSize: 15,
+                  ),
+                  primary: AppTheme.lightBackground,
+                  backgroundColor: AppTheme.darkBackground,
                 ),
               ),
-            ]),
-            TableRow(children: [
-              Text(
-                "Composer",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editComposer,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editComposer = value!;
-                  });
-                },
-              ),
-              TextField(
-                textAlign: TextAlign.left,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'No Composer',
-                ),
-              ),
-            ]),
-            TableRow(children: [
-              Text(
-                "Genres",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editGenres,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editGenres = value!;
-                  });
-                },
-              ),
-              TextField(
-                textAlign: TextAlign.left,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'No Genres',
-                ),
-              ),
-            ]),
-            TableRow(children: [
-              Text(
-                "Tags",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editTags,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editTags = value!;
-                  });
-                },
-              ),
-              TextField(
-                textAlign: TextAlign.left,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'No Tags',
-                ),
-              ),
-            ]),
-            TableRow(children: [
-              Text(
-                "Labels",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editLabels,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editLabels = value!;
-                  });
-                },
-              ),
-              TextField(
-                textAlign: TextAlign.left,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'No Labels',
-                ),
-              ),
-            ]),
-            TableRow(children: [
-              Text(
-                "Reference",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editReference,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editReference = value!;
-                  });
-                },
-              ),
-              TextField(
-                textAlign: TextAlign.left,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'No Reference',
-                ),
-              ),
-            ]),
-            TableRow(children: [
-              Text(
-                "Rating",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editRating,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editRating = value!;
-                  });
-                },
-              ),
-              RatingBar.builder(
-                initialRating: 0,
-                minRating: 0,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemBuilder: (context, _) => Icon(
-                  Icons.star,
-                  color: AppTheme.accentMain,
-                ),
-                onRatingUpdate: (rating) {},
-              ),
-            ]),
-            TableRow(children: [
-              Text(
-                "Difficulty",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editDifficulty,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editDifficulty = value!;
-                  });
-                },
-              ),
-              RatingBar.builder(
-                initialRating: 0,
-                minRating: 0,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 3,
-                itemBuilder: (context, _) => Icon(
-                  Icons.circle,
-                  color: AppTheme.accentMain,
-                ),
-                onRatingUpdate: (rating) {},
-              ),
-            ]),
-            TableRow(children: [
-              Text(
-                "Time",
-                textAlign: TextAlign.right,
-                textScaleFactor: 1.3,
-              ),
-              Checkbox(
-                activeColor: AppTheme.accentMain,
-                value: editTime,
-                onChanged: (bool? value) {
-                  setState(() {
-                    editTime = value!;
-                  });
-                },
-              ),
-              TextField(
-                textAlign: TextAlign.left,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'No Time',
-                ),
-              ),
-            ]),
-          ],
+            ],
+          ),
         ),
       ),
     );
