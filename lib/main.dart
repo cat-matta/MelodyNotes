0c996b568ee9f4933be0e86b04d27cb237dde2af
Catherine <catherine.matta22399@gmail.com>
2022-11-17 11:52:45 -0500

    Added changing tabs, needs further debugging

--- lib/main.dart
+++ lib/main.dart
@@ -46,17 +46,46 @@ class AppEntry extends StatelessWidget {
   }
 }
 
-class TopBar extends StatefulWidget {
+class TopBar extends ConsumerStatefulWidget {
   @override
-  State<TopBar> createState() => _TopBarState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _TopBarState();
 }
 
-class _TopBarState extends State<TopBar> {
+class _TopBarState extends ConsumerState<TopBar> {
+  late List<Score> _currentScores;
+  @override
+  void initState() {
+    super.initState();
+    setup();
+  }
+
+  void setup() async {
+    super.initState();
+    final prefs = await SharedPreferences.getInstance();
+    final int? fileId = prefs.getInt('file_id');
+    final List<String>? items = prefs.getStringList('saved_scores');
+    if (fileId == null || items == null)
+      return;
+    else {
+      ScoreService servObj = ScoreService();
+      List<Score> listsOfScores = await servObj.getAllScores();
+
+      Score cachedScore = ref
+          .read(scoresListProvider.notifier)
+          .getScorefromID(listsOfScores, fileId);
+      print("cached score id: $fileId");
+      ref.read(pdfFileProvider.notifier).giveFile(cachedScore);
+      ref.read(currentScoresListProvider.notifier).getCache(items);
+      print("Cached items: ${items}");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final mediaQuerry = MediaQuery.of(context);
     List<VoidCallback> menuFunctions = [];
-
+    _currentScores = ref.watch(currentScoresListProvider);
+    int numTabs = _currentScores.length;
     bool isDesktop(BuildContext context) {
       return mediaQuerry.size.width >= 700;
     }
@@ -113,54 +142,69 @@ class _TopBarState extends State<TopBar> {
       );
     }
 
-    return Scaffold(
-        appBar: AppBar(
-            centerTitle: true,
-            titleSpacing: 0.0,
-            title: ScoreTitle(),
-            leadingWidth: mediaQuerry.orientation == Orientation.landscape ||
-                    isDesktop(context)
-                ? mediaQuerry.size.width * 0.25
-                : mediaQuerry.size.width * 0.15,
-            leading: mediaQuerry.orientation == Orientation.landscape ||
-                    isDesktop(context)
-                ? Row(
-                    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      buildAppBarIcons(Icons.photo_camera, () {}),
-                      buildAppBarIcons(Icons.draw, () {}),
-                      buildAppBarIcons(Icons.display_settings, () {}),
-                      buildAppBarIcons(Icons.collections_bookmark, () {}),
-                    ],
-                  )
-                : buildAppBarIcons(Icons.draw, () {}),
-            actions: [
-              buildAppBarIcons(Icons.library_music, () {
-                showScoreDrawer(
-                  context,
-                  ScoreDrawer(),
-                  0.7,
-                );
-              }),
-              buildAppBarIcons(Icons.music_note, () {
-                showSetlistDrawer(
-                  context,
-                  SetlistDrawer(),
-                  0.7,
-                );
-              }),
-              buildAppBarIcons(Icons.bookmark, () {
-                showBookMarkDrawer(
-                  context,
-                  BookMarkDrawer(),
-                  0.7,
-                );
-              }),
-              buildAppBarIcons(Icons.menu, () {
-                showMainDrawer(context, MainDrawer(), 0.7);
-              }),
-            ]),
-        body: AppBody());
+    return DefaultTabController(
+      length: _currentScores.length,
+      child: Scaffold(
+          appBar: AppBar(
+              bottom: TabBar(
+                  indicatorColor: AppTheme.accentMain,
+                  unselectedLabelColor: AppTheme.lightBackground,
+                  labelColor: AppTheme.accentMain,
+                  onTap: (value) {
+                    ref
+                        .read(pdfFileProvider.notifier)
+                        .giveFile(_currentScores[value]);
+                  },
+                  tabs: _currentScores
+                      .map((element) => Text(element.name))
+                      .toList()),
+              centerTitle: true,
+              titleSpacing: 0.0,
+              title: ScoreTitle(),
+              leadingWidth: mediaQuerry.orientation == Orientation.landscape ||
+                      isDesktop(context)
+                  ? mediaQuerry.size.width * 0.25
+                  : mediaQuerry.size.width * 0.15,
+              leading: mediaQuerry.orientation == Orientation.landscape ||
+                      isDesktop(context)
+                  ? Row(
+                      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildAppBarIcons(Icons.photo_camera, () {}),
+                        buildAppBarIcons(Icons.draw, () {}),
+                        buildAppBarIcons(Icons.display_settings, () {}),
+                        buildAppBarIcons(Icons.collections_bookmark, () {}),
+                      ],
+                    )
+                  : buildAppBarIcons(Icons.draw, () {}),
+              actions: [
+                buildAppBarIcons(Icons.library_music, () {
+                  showScoreDrawer(
+                    context,
+                    ScoreDrawer(),
+                    0.7,
+                  );
+                }),
+                buildAppBarIcons(Icons.music_note, () {
+                  showSetlistDrawer(
+                    context,
+                    SetlistDrawer(),
+                    0.7,
+                  );
+                }),
+                buildAppBarIcons(Icons.bookmark, () {
+                  showBookMarkDrawer(
+                    context,
+                    BookMarkDrawer(),
+                    0.7,
+                  );
+                }),
+                buildAppBarIcons(Icons.menu, () {
+                  showMainDrawer(context, MainDrawer(), 0.7);
+                }),
+              ]),
+          body: AppBody()),
+    );
   }
 }
 
@@ -285,85 +329,97 @@ class _AppBodyState extends ConsumerState<AppBody> {
   @override
   Widget build(BuildContext context) {
     var currentFile = ref.watch(pdfFileProvider);
+    List<Score> _currentScores = ref.watch(currentScoresListProvider);
+    return TabBarView(
+        children: _currentScores.map((score) {
+      return OrientationBuilder(
+          builder: (BuildContext context, Orientation orientation) {
+        if (orientation == Orientation.landscape)
+          return SfPdfViewer.file(File(score.file));
+        else
+          return SfPdfViewer.file(File(score.file),
+              pageLayoutMode: PdfPageLayoutMode.single);
+      });
+    }).toList());
 
-    return currentFile.when(
-        data: (currentFile) {
-          // return Text("$currentFile");
-          print("Currently on: $currentFile");
+    // return currentFile.when(
+    //     data: (currentFile) {
+    //       // return Text("$currentFile");
+    //       print("Currently on: $currentFile");
 
-          return OrientationBuilder(
-              builder: (BuildContext context, Orientation orientation) {
-            if (orientation == Orientation.landscape)
-              return SfPdfViewer.file(File(currentFile.file));
-            else
-              return SfPdfViewer.file(File(currentFile.file),
-                  pageLayoutMode: PdfPageLayoutMode.single);
-          });
-        },
-        error: ((error, stackTrace) => Text("Err: $error")),
-        loading: () => Center(
-              child: Container(
-                  height: MediaQuery.of(context).size.height,
-                  decoration: BoxDecoration(color: AppTheme.darkBackground),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Spacer(),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.music_note),
-                          Icon(Icons.music_note),
-                          Icon(Icons.music_note),
-                        ],
-                      ),
-                      Spacer(),
-                      RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text:
-                                  "Welcome to MelodyScore! You can import a file by clicking on the ",
-                              style: TextStyle(
-                                color: AppTheme.accentMain,
-                                fontSize: 20,
-                              ),
-                            ),
-                            WidgetSpan(
-                              child: Icon(Icons.library_music, size: 20),
-                            ),
-                            TextSpan(
-                              text: " above",
-                              style: TextStyle(
-                                color: AppTheme.accentMain,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Text(
-                        "Happy Playing!",
-                        style:
-                            TextStyle(color: AppTheme.accentMain, fontSize: 20),
-                      ),
-                      Spacer(),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.music_note),
-                          Icon(Icons.music_note),
-                          Icon(Icons.music_note),
-                        ],
-                      ),
-                      Spacer(),
-                    ],
-                  )),
-            ));
+    //       return OrientationBuilder(
+    //           builder: (BuildContext context, Orientation orientation) {
+    //         if (orientation == Orientation.landscape)
+    //           return SfPdfViewer.file(File(currentFile.file));
+    //         else
+    //           return SfPdfViewer.file(File(currentFile.file),
+    //               pageLayoutMode: PdfPageLayoutMode.single);
+    //       });
+    //   //     },
+    //       error: ((error, stackTrace) => Text("Err: $error")),
+    //       loading: () => Center(
+    //             child: Container(
+    //                 height: MediaQuery.of(context).size.height,
+    //                 decoration: BoxDecoration(color: AppTheme.darkBackground),
+    //                 child: Column(
+    //                   mainAxisAlignment: MainAxisAlignment.center,
+    //                   crossAxisAlignment: CrossAxisAlignment.center,
+    //                   children: [
+    //                     Spacer(),
+    //                     Row(
+    //                       crossAxisAlignment: CrossAxisAlignment.center,
+    //                       mainAxisAlignment: MainAxisAlignment.center,
+    //                       children: [
+    //                         Icon(Icons.music_note),
+    //                         Icon(Icons.music_note),
+    //                         Icon(Icons.music_note),
+    //                       ],
+    //                     ),
+    //                     Spacer(),
+    //                     RichText(
+    //                       textAlign: TextAlign.center,
+    //                       text: TextSpan(
+    //                         children: [
+    //                           TextSpan(
+    //                             text:
+    //                                 "Welcome to MelodyScore! You can import a file by clicking on the ",
+    //                             style: TextStyle(
+    //                               color: AppTheme.accentMain,
+    //                               fontSize: 20,
+    //                             ),
+    //                           ),
+    //                           WidgetSpan(
+    //                             child: Icon(Icons.library_music, size: 20),
+    //                           ),
+    //                           TextSpan(
+    //                             text: " above",
+    //                             style: TextStyle(
+    //                               color: AppTheme.accentMain,
+    //                               fontSize: 20,
+    //                             ),
+    //                           ),
+    //                         ],
+    //                       ),
+    //                     ),
+    //                     Spacer(),
+    //                     Text(
+    //                       "Happy Playing!",
+    //                       style:
+    //                           TextStyle(color: AppTheme.accentMain, fontSize: 20),
+    //                     ),
+    //                     Spacer(),
+    //                     Row(
+    //                       crossAxisAlignment: CrossAxisAlignment.center,
+    //                       mainAxisAlignment: MainAxisAlignment.center,
+    //                       children: [
+    //                         Icon(Icons.music_note),
+    //                         Icon(Icons.music_note),
+    //                         Icon(Icons.music_note),
+    //                       ],
+    //                     ),
+    //                     Spacer(),
+    //                   ],
+    //                 )),
+    //           ));
   }
 }
