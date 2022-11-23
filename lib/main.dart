8019066489cffc8cba658e66881eec8e6ee4319b
Catherine <catherine.matta22399@gmail.com>
2022-11-23 00:46:45 -0500

    Added toggle for showing tabs

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ import 'themedata.dart';
 
 late List<CameraDescription> cameras;
 late List<Score> _currentScores;
+bool tabsToggle = false;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await injector.setupLocator();
@@ -156,7 +158,10 @@ class _TopBarState extends ConsumerState<TopBar> {
                     children: [
                       buildAppBarIcons(Icons.photo_camera, () {}),
                       buildAppBarIcons(Icons.draw, () {}),
-                      buildAppBarIcons(Icons.display_settings, () {}),
+                      buildAppBarIcons(Icons.display_settings, () {
+                        tabsToggle = !tabsToggle;
+                        setState(() {});
+                      }),
                       buildAppBarIcons(Icons.collections_bookmark, () {}),
                     ],
                   )
@@ -316,62 +321,154 @@ class _AppBodyState extends ConsumerState<AppBody> {
     var currentFile = ref.watch(pdfFileProvider);
     _currentScores = ref.watch(currentScoresListProvider);
     int numTabs = _currentScores.length;
-    return DefaultTabController(
-      length: numTabs,
-      child: Container(
-        child: Scaffold(
-            appBar: PreferredSize(
-              preferredSize: mediaQuerry.size / 20,
-              child: AppBar(
-                  bottom: TabBar(
-                      isScrollable: true,
-                      indicatorColor: AppTheme.accentMain,
-                      unselectedLabelColor: AppTheme.lightBackground,
-                      labelColor: AppTheme.accentMain,
-                      onTap: (value) {
-                        ref
-                            .read(pdfFileProvider.notifier)
-                            .giveFile(_currentScores[value]);
-                      },
-                      tabs: _currentScores
-                          .map((element) => Padding(
-                                padding: const EdgeInsets.all(1.0),
-                                child: Flexible(
-                                  fit: FlexFit.tight,
-                                  child: Row(children: [
-                                    Text(
-                                      element.name,
-                                      style: TextStyle(
-                                          overflow: TextOverflow.ellipsis),
-                                    ),
-                                    IconButton(
-                                      icon: Icon(Icons.close),
-                                      onPressed: () {
-                                        ref
-                                            .read(currentScoresListProvider
-                                                .notifier)
-                                            .removeScore(element);
-                                        setState(() {});
-                                      },
-                                    )
-                                  ]),
-                                ),
-                              ))
-                          .toList())),
+    return tabsToggle
+        ? DefaultTabController(
+            length: numTabs,
+            child: Container(
+              child: Scaffold(
+                  appBar: PreferredSize(
+                    preferredSize: mediaQuerry.size / 20,
+                    child: AppBar(
+                        bottom: TabBar(
+                            isScrollable: true,
+                            indicatorColor: AppTheme.accentMain,
+                            unselectedLabelColor: AppTheme.lightBackground,
+                            labelColor: AppTheme.accentMain,
+                            onTap: (value) {
+                              ref
+                                  .read(pdfFileProvider.notifier)
+                                  .giveFile(_currentScores[value]);
+                              ref
+                                  .read(pdfFileProvider.notifier)
+                                  .setCache(value);
+                              setState(() {});
+                            },
+                            tabs: _currentScores
+                                .map((element) => Padding(
+                                      padding: const EdgeInsets.all(1.0),
+                                      child: Flexible(
+                                        fit: FlexFit.tight,
+                                        child: Row(children: [
+                                          Text(
+                                            element.name,
+                                            style: TextStyle(
+                                                overflow:
+                                                    TextOverflow.ellipsis),
+                                          ),
+                                          IconButton(
+                                            icon: Icon(Icons.close),
+                                            onPressed: () {
+                                              ref
+                                                  .read(
+                                                      currentScoresListProvider
+                                                          .notifier)
+                                                  .removeScore(element);
+                                              setState(() {});
+                                            },
+                                          )
+                                        ]),
+                                      ),
+                                    ))
+                                .toList())),
+                  ),
+                  body: TabBarView(
+                      children: _currentScores.map((score) {
+                    return PDFPage(
+                      score: score,
+                    );
+                  }).toList())),
             ),
-            body: TabBarView(
-                children: _currentScores.map((score) {
-              return OrientationBuilder(
-                  builder: (BuildContext context, Orientation orientation) {
-                if (orientation == Orientation.landscape)
-                  return SfPdfViewer.file(File(score.file));
-                else
-                  return SfPdfViewer.file(File(score.file),
-                      pageLayoutMode: PdfPageLayoutMode.single);
-              });
-            }).toList())),
-      ),
-    );
+          )
+        : currentFile.when(
+            data: (currentFile) {
+              // print("Currently on: $currentFile");
+
+              return PDFPage(score: currentFile);
+            },
+            error: ((error, stackTrace) => Text("Err: $error")),
+            loading: () => Center(
+                  child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(color: AppTheme.darkBackground),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Spacer(),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.music_note),
+                              Icon(Icons.music_note),
+                              Icon(Icons.music_note),
+                            ],
+                          ),
+                          Spacer(),
+                          RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text:
+                                      "Welcome to MelodyScore! You can import a file by clicking on the ",
+                                  style: TextStyle(
+                                    color: AppTheme.accentMain,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                WidgetSpan(
+                                  child: Icon(Icons.library_music, size: 20),
+                                ),
+                                TextSpan(
+                                  text: " above",
+                                  style: TextStyle(
+                                    color: AppTheme.accentMain,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Spacer(),
+                          Text(
+                            "Happy Playing!",
+                            style: TextStyle(
+                                color: AppTheme.accentMain, fontSize: 20),
+                          ),
+                          Spacer(),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.music_note),
+                              Icon(Icons.music_note),
+                              Icon(Icons.music_note),
+                            ],
+                          ),
+                          Spacer(),
+                        ],
+                      )),
+                ));
+  }
+}
+
+class PDFPage extends StatelessWidget {
+  const PDFPage({
+    Key? key,
+    required this.score,
+  }) : super(key: key);
+  final Score score;
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) {
+      if (orientation == Orientation.landscape)
+        return SfPdfViewer.file(File(score.file));
+      else
+        return SfPdfViewer.file(File(score.file),
+            pageLayoutMode: PdfPageLayoutMode.single);
+    });
   }
 }
   // return TabBarView(
@@ -386,7 +483,9 @@ class _AppBodyState extends ConsumerState<AppBody> {
   //   });
   // }).toList());
 
-  // return currentFile.when(
+  
+
+// return currentFile.when(
   //     data: (currentFile) {
   //       print("Currently on: $currentFile");
 
@@ -465,4 +564,3 @@ class _AppBodyState extends ConsumerState<AppBody> {
   //               )),
   //         ));
   // }
-
